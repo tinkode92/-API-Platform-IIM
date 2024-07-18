3b7967a3cfdee69c20076ef98ebe146a2e4bbe25
tinkode92 <yanis.bekrarchouche@edu.devinci.fr>
2024-07-18 12:35:21 +0200

    add security controller

---
+++ my_bar_project/src/Controller/SecurityController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="app_login")
+     */
+    public function login(): Response
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        // Symfony gère la déconnexion automatiquement
+    }
+}
